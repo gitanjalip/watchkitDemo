3afb1d91b69b5b72c49e970c88c386ee8243a32b
Seriapin 3 <narsingiyanile@narsings-Mac-mini.local>
2015-07-13 17:30:46 +0530

    Initial Commit

---
+++ watchkitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  watchkitDemo
+//
+//  Created by Seriapin 3 on 13/07/15.
+//  Copyright (c) 2015 Seriapin 3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ watchkitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  watchkitDemo
+//
+//  Created by Seriapin 3 on 13/07/15.
+//  Copyright (c) 2015 Seriapin 3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
